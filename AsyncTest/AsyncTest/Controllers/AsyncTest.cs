72ada5380b36f1ce4e0178084d0e9aa8958c0167
Brian Desiderio <bdesiderio@gruposabra.com>
2020-01-09 17:21:17 -0300

    Se sube proyecto

---
+++ AsyncTest/AsyncTest/Controllers/AsyncTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AsyncTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AsyncTest : ControllerBase
+    {
+        [HttpGet("noawait")]
+        public async Task<IActionResult> GetNoAwait()
+        {
+            MethodAsync();
+            return Ok("Todo en Orden");
+        }
+
+        [HttpGet("await")]
+        public async Task<IActionResult> GetAwait()
+        {
+            await MethodAsync();
+            return Ok("Todo en Orden");
+        }
+
+        private async Task MethodAsync()
+        {
+            await Task.Delay(5000);
+            System.Diagnostics.Debug.WriteLine("Se ejecuto el Delay");
+        }
+    }
+}

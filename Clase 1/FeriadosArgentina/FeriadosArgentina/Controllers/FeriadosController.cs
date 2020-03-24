fcb695c48cd36bb6b39988e284611a89d694928f
Brian Desiderio <bdesiderio@gruposabra.com>
2020-03-24 18:00:30 -0300

    Se sube Clase 1

---
+++ Clase 1/FeriadosArgentina/FeriadosArgentina/Controllers/FeriadosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeriadosArgentina.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeriadosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (FileStream fs = new FileStream("data.json", FileMode.Open))
+            {
+                using (StreamReader streamReader = new StreamReader(fs))
+                {
+                    var data = await streamReader.ReadToEndAsync();
+                    return Ok(data);
+                }
+            }
+        }
+    }
+}
